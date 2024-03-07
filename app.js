cc99c1e275aec4beff75d3fc66dd50e964435bf1
abdulraheemghani@gmail.com <abdulraheemghani0@gmail.com>
2024-03-07 13:25:37 -0800

    logics exercise

---
+++ app.js
@@ -0,0 +1,280 @@
+
+
+
+//CHAPTER #1
+
+// var firstName=("Abdul");
+// var LastName=("Raheem Ghani");
+// var age =("18");
+// var school =("SMIT");
+
+
+
+
+
+
+// Question No.1
+// write ascript to greet your website visitor using js alert box
+
+//Answer =
+  // alert("Welcome to JS land  happy coding!");
+
+
+
+
+
+
+
+
+//Question no.02
+//write a script to display following message on your website.
+
+//Answer =
+  // alert("Error! please enter a valid password");
+
+
+
+
+
+
+
+
+//Question no.03
+//	Write a script to display following message on your web page: (Hint : Use line break).
+
+//Answer = 03
+
+ //alert("Welcome to JS land...\n  happy coding!");
+
+
+
+ //Question no.04
+ //⦁	Write a script to display following messages in sequence:
+ 
+ //Answer = 04
+ //alert("Welcome to JS land  happy coding!");
+ //alert("Error! please enter a valid password");
+ //alert("Welcome to JS land...\n  happy coding!");
+
+
+
+//Question no.05
+ //Generate following message thorugh browser's devoloper console.
+ 
+ //Answer = 05
+//console.log("Hello. i can run JS through my browsers console");
+
+
+//          CHAPTER NO.1   END
+ 
+
+//           CHAPTER NO.2   START
+
+
+//  Q.NO.01
+//  var userName = ("AbdulRaheem");
+//  Q.NO.02
+//  var myName = ("Abdul Raheem Ghani");
+//  Q.NO.03
+//  var message = ("Hello World");
+//  alert(message);
+
+// Q.NO.04
+//   alert(userName+" I am learning web and app      development")
+
+// Q.NO.05
+// display alert message.
+//Answer
+// alert("PIZZA \n PIZZ \n PIZ \n PIZ \n P ")
+ 
+
+
+
+// Q.NO.06
+// Declair a variable  called email and assingn it a string for email address show in alert box.
+//
+// var email = ("abdulraheemghani0@gmail.com");
+// alert("My email address is " +email);
+
+
+
+// Q.NO.06
+// Declair a variable  called book and assingn it a string for bookname show in alert box.
+//
+//  var book = ("a smarter way to learn JavaScript");
+// alert("I am trying to learn javascript from book " +book);
+
+
+
+
+
+
+
+// Q.NO.07
+// write a sript shoew in browser through js.
+//
+//    document.write("yea! I can write text throuhg JS");
+
+
+// Q.NO.08
+//Store following string in a variable show in alert .
+//
+//alert("----------<#####>-------------");
+
+
+
+
+//          CHAPTER .NO.02 END
+
+//          CHAPTER .NO.03 STARTED
+
+
+
+// Q.NO.01
+//Declair a variable age  show in alert .
+// var age = (18);
+// alert("I am " + age + " years old");
+
+// Q.NO.02
+//declair a variable how many times a visitor visits a web page,show in alert .
+//var visits = ("14");
+//alert("you have visited the site"+visits+" times  ")
+//
+
+
+//Q.NO.03
+//Declair a variable called birth year ,shows it in your browser.
+// var birthYear = (2005);
+// document.write ("my birth year is "+birthYear)
+
+
+
+
+
+
+//Q.NO.04
+//Declair  variables for product, quantity, visitor name and shows it in browser  .
+//
+//var visitorName = ("Usman ");
+//var product = ("Toys ");
+//var quantity = ("10 ");
+//document.write(visitorName + "ordered" + quantity + product + "from Daraz.com");
+
+
+
+//             CHAPTER.NO.3 END
+
+
+//             CHAPTER.NO.4 STARTED
+
+//Q.NO.01
+//Declair 3 variables in one statement   .
+
+
+//var intro = ("My name is ");
+// var firstName = ("Abdul raheem ");
+// var LastName = ("Ghani ");
+
+// document.write(intro + firstName + LastName );
+
+
+
+
+
+
+//Q.NO.02
+//Declare 5 legal and 5 illegal variable names.
+// document.write("we can use these variables name 1.firstname | 2.FirstName | 3.lastName123 | 4.$xyz | 5.first123Name");
+
+
+// document.write("we can't use these variables name 1.123firstname | 2.@FirstName | 3.#lastName123 | 4.!xyz | 5.http//");
+
+
+
+
+//Q.NO.02
+//Display this in your browser.
+
+// document.write(`<h1>Rules for naming js variables</h1> \n <br> <h4>variable names can only contain ,numbers , $ and -.for example :$my-1stvariable<br>Variable must being with a letteer , $ or _ .For example:$name,-name or name<br> Variable name are case sensitive <br> Variable names should not be JS key words</h4>`)
+
+
+
+
+
+//          CHAPTER NO.04 END
+
+
+//          CHAPTER NO.05 STARTED
+
+
+//Q.NO.01
+//add two numbers and display on browser.
+// var num1 = (10)
+// var num2 = (20)
+// var Number = (num1+num2)
+//  document.write("sum of "+num1+"and "+num2+" is equal to "+Number)
+
+
+
+
+
+
+//Q.NO.02
+// repeate for subtraction,multiplication , division ,modulus....
+
+// var num1 = (10)
+// var num2 = (20)
+// var Number = (num1-num2)
+//  document.write("subtraction of "+num1+"and "+num2+" is equal to "+Number)
+
+
+
+// var num1 = (10)
+//  var num2 = (20)
+//  var Number = (num1*num2)
+//   document.write("multiplication of "+num1+"and "+num2+" is equal to "+Number)
+
+
+
+  // var num1 = (10)
+  // var num2 = (20)
+  // var Number = (num1/num2)
+  //  document.write("division of "+num1+"and "+num2+" is equal to "+Number)
+ 
+
+
+// var num1 = (20)
+//   var num2 = (2)
+//   var Number = (num1%num2)
+//    document.write("modulus of "+num1+"and "+num2+" is equal to "+Number)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
