97d803f9dc93e2c4433917226b08d7bdbee1d546
Abdul Raheem Ghani <abdulraheemghani0@gmail.com>
2024-03-26 23:54:27 +0500

    Add files via upload

---
+++ loop exersices/app.js
@@ -0,0 +1,153 @@
+
+
+// Q.no.01
+//  write a program to display "hello world " five times in your browser.
+
+
+// const h1= document.querySelector("#head")
+// for (let i = 1; i <= 5; i++) {
+//    let stg =("Hello World")
+//     h1.innerHTML +=(`<li>${stg}</li>`)    
+// }
+ // ----------------------------------------------------
+// Q.NO.02
+//  write a program to print numaric counting 1-10.
+
+
+// const h1= document.querySelector("#head")
+//  for (let i = 1; i <= 5; i++) {
+   
+//    h1.innerHTML +=(`<li>${i}</li>`)   
+//  }
+
+//--------------------------------------------------------
+
+//Q.NO.03
+//  write a program to print Manuplation table of any number using for loop .table number and lenght should be given by user ..
+
+//  let num1 =+prompt("");
+//  let num2 =+prompt ("");
+// for (let i = 1; i <=num2 ; i++) {
+//     // console.log(`${nm1} * ${i} = ${num1*i}`);
+//     console.log(` ${num1} * ${i} = ${num1*i}`)
+// }
+
+
+//---------------------------------------------------------
+// Q.no.04
+//   Write each element on the new line with help of for loop
+
+// let array =[`Nokia `, `Sumsung`,`apple `, `Sony` ,`Huawei`]
+
+
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+    
+// }
+
+//----------------------------------------------------------
+
+// Q.No.05
+//write a program to write the element of the following elements for loop;
+
+//  let array =[`Apple  `, `Banana`,`Mango`, `Orange` ,`Strawberry`]
+//  for (let i = 0; i < array.length; i++) {
+//      console.log(array[i]);
+//  }    
+//  for (let i = 0; i < array.length; i++) {
+//   console.log(`Element of index ${i} is ${array[i]}`)
+// }  
+
+// ----------------------------------------------------------
+//Q.no.06
+// write  a program to innitialize a array of N items by using prompt .Where  N is a number of items as entered  by user ;
+
+// let numN = +prompt("enter number of items ");
+//  let array =[``] ;
+// console.log(`number of items is ==>${numN}`);
+// for (let i = 1; i <= numN; i++) {
+//   let num1 = prompt(`enter value of index ${i}`);
+// array.push(num1);
+// console.log(array[i]);
+// };
+
+
+
+//------------------------------------------------------------
+
+// Q.no.07
+// Generate the following series in your browser . 
+
+
+
+// document.write(`<li>counting:</li>`);
+// for (let i = 1; i <= 15 ; i++) {
+//     document.write(`${i},`)
+// }
+
+
+
+// document.write(`<li>Reverse-counting:</li>`);
+// for (let i = 15; i >= 1 ; i--) {
+//     document.write(`${i},`)
+// }
+// document.write(`<li>Even
+// :</li>`);
+// for (let i = 0; i <= 15; i += 2) {
+//    document.write(`${i},`)
+
+// }
+
+
+// document.write(`<li>Odd:</li>`);
+// for (let i = 0; i <= 15; i += 3) {
+//    document.write(`${i},`)
+
+// }
+
+// document.write(`<li>series:</li>`);
+// for (let i = 2; i <= 15; i += 2 ) {
+//    document.write(`${i}k,`)
+
+// }
+
+
+//-------------------------------------------------------
+// Q.no.08
+//  write a program to enable "search by user input " in an array .
+
+
+// let array=["cake", "apple pie","cookie","chips","patties"]
+// function search(item) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === item) {
+//             return true;  } }
+//     return false;
+// }
+// let items = prompt("Enter an item to search:");
+// if (items) {
+//     if (search(items.toLowerCase())) {
+//         console.log(items + ` is available.`);
+//     } else {
+//         console.log(items + ` is not available.`);
+//     }
+// } 
+
+
+// -----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
